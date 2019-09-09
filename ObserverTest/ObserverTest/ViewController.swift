435588509788ba650ff5490356e9b741f1f262f0
Hyowon Kim <hyopangworld@gmail.com>
2019-09-09 11:08:09 +0900

    add Observer Test Project

---
+++ ObserverTest/ObserverTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObserverTest
+//
+//  Created by 김효원 on 09/09/2019.
+//  Copyright © 2019 HyowonKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
