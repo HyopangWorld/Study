ef278fcc6da6f19cf3da35a4d797ea89d779b5e3
Hyowon Kim <hyopangworld@gmail.com>
2019-09-19 16:14:17 +0900

    [MVVMSwiftExample] player view ViewModel 추가

---
+++ MVVMSwiftExample/MVVMSwiftExample/PlayerScoreboardMoveEditorViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  PlayerScoreboardMoveEditorViewModel.swift
+//  MVVMSwiftExample
+//
+//  Created by 김효원 on 19/09/2019.
+//  Copyright © 2019 Toptal. All rights reserved.
+//
+
+import Foundation
+
+protocol PlayerScoreboardMoveEditorViewModel {
+    var playerName: String { get }
+    
+    var onePointMoveCount: String { get }
+    var twoPointMoveCount: String { get }
+    var assistMoveCount: String { get }
+    var reboundMoveCount: String { get }
+    var foulMoveCount: String { get }
+    
+    func onePointMove()
+    func twoPointMove()
+    func assistMove()
+    func reboundMove()
+    func foulMove()
+}
